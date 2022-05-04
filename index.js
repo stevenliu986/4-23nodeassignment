b3312eef46c39de0e8768254a287ce584382bf19
StevenLiu <stevenliu986@gmail.com>
2022-05-04 23:50:13 +1000

    更新

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ const PORT = 3000;
 // create a new task
 app.post("/tasks", (req, res) => {
   const { description } = req.body;
-  tasks.push({ id: id++, description: description, done: false });
-  res.status(201).json("task added successfully");
+  const tempObj = { id: id++, description: description, done: false };
+  tasks.push(tempObj);
+  res.status(201).json(tempObj);
 });
 
 // get all tasks
@@ -24,12 +25,30 @@ app.get("/tasks", (req, res) => {
 // get task by id
 app.get("/tasks/:id", (req, res) => {
   const result = tasks.find((task) => task.id === +req.params.id);
-  res.send(result);
+  res.json(result);
 });
 
 // update task by id
+app.put("/tasks/:id", (req, res) => {
+  const { description, done } = req.body;
+  // 判断description的值是否为空
+  if (!description || !done) {
+    res.status(404);
+    return;
+  }
+  const result = tasks.find((task) => task.id === +req.params.id);
+  result["description"] = description;
+  result["done"] = true;
+  res.status(200).json(result);
+});
 
 // delete task by id
+app.delete("/tasks/:id", (req, res) => {
+  const index = tasks.findIndex((tasks) => tasks.id === +req.params.id);
+  res.send(index);
+  // tasks.splice(index, 1);
+  // res.status(204);
+});
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
